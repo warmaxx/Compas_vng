2547c65b6235074d6f06456f6ea8968571f73c75
warmaxx <warmaxx@ya.ru>
2021-04-28 11:09:11 +0300

    refactor query in index , stat

--- vng_stat/views.py
+++ vng_stat/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from homepage.models import FedRegion, Region
 from .models import Check, Tek_Object, Ul, Form, Category
 
-def index(request):
 
-    checks = Check.objects.all()
+def index(request):
+    checks = Check.objects.all().values(
+        'tek_Object__region__name',
+        'tek_Object__region__fedname',
+        'id',
+        'tek_Object__name',
+        'tek_Object__place',
+        'tek_Object__category__name',
+        'date_latest_check',
+        'date_next_check',
+        'duration',
+        'form__name',
+        'target_link',
+        'target_text',
+        'tek_Object__ul__name',
+        'tek_Object__ul__place',
+        'tek_Object__ul__OGRN',
+        'tek_Object__ul__INN'
+    )
 
     context = {
 
@@ -13,20 +30,33 @@ def index(request):
 
     return render(request, 'vng_stat/index.html', context)
 
-def stat (request):
 
-    checks = Check.objects.all()
-    fedregions = FedRegion.objects.all()
-    regions = Region.objects.all()
-
-    p = checks.count()
-
-
+def stat(request):
+    checks = Check.objects.all().values(
+        'tek_Object__region__name',
+        'tek_Object__region__fedname',
+        'id',
+        'tek_Object__name',
+        'tek_Object__place',
+        'tek_Object__category__name',
+        'date_latest_check',
+        'date_next_check',
+        'duration',
+        'form__name',
+        'target_link',
+        'target_text',
+        'tek_Object__ul__name',
+        'tek_Object__ul__place',
+        'tek_Object__ul__OGRN',
+        'tek_Object__ul__INN'
+    )
+    fedregions = FedRegion.objects.all().values('id', 'name')
+    regions = Region.objects.all().values('id', 'name')
+    p = checks.filter(tek_Object__category__name='Низкий').count()
     context = {
         'p':p,
         'checks': checks,
-        'fedregions':fedregions,
-        'regions':regions,
+        'fedregions': fedregions,
+        'regions': regions,
     }
     return render(request, 'vng_stat/stat.html', context)
-
