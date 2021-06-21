59086df9f216e9f353c7d46bacc0b56004d8e31f
Maksyagin Aleksey <warmaxx@ya.ru>
2021-06-21 23:40:57 +0300

    reverse ordering news

--- homepage/views.py
+++ homepage/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from vng_stat.models import Tek_Object, Ul
 from news.models import News
+from datetime import datetime as dt
 
 
 def index(request):
+    now = dt.now()
     count_ul = Ul.objects.count()
     count_all = Tek_Object.objects.count()
     count_high = Tek_Object.objects.filter(category__name='Высокая').count()
     count_medium = Tek_Object.objects.filter(category__name='Средняя').count()
     count_low = Tek_Object.objects.filter(category__name='Низкая').count()
-    news = News.objects.all()
+    news = News.objects.filter(date_start__lte=now).filter(date_end__gte=now).order_by('id').reverse()
 
     context = {
         'count_ul': count_ul,
