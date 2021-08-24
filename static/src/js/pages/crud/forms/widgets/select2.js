b46d78ea904f4b18283ddc970453f492c7914427
Maksyagin Aleksey <warmaxx@ya.ru>
2021-08-24 11:11:07 +0300

    vng_info add autofill select2

--- static/src/js/pages/crud/forms/widgets/select2.js
+++ static/src/js/pages/crud/forms/widgets/select2.js
@@ -3,14 +3,17 @@ var KTSelect2 = function() {
     // Private functions
     var demos = function() {
         // basic
-        $('#kt_select2_1, #kt_select2_1_validate').select2({
-            placeholder: 'Select a state'
+        // $('#kt_select2_1, #kt_select2_1_validate').select2({
+        //     placeholder: 'Select a state'
+        // });
+        $('#kt_select2_1, #kt_select2_2').select2({
+            placeholder: 'Выберите регион'
         });
 
         // nested
-        $('#kt_select2_2, #kt_select2_2_validate').select2({
-            placeholder: 'Select a state'
-        });
+        // $('#kt_select2_2, #kt_select2_2_validate').select2({
+        //     placeholder: 'Select a state'
+        // });
 
         // multi select
         $('#kt_select2_3, #kt_select2_3_validate').select2({
