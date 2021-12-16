6fa4e4943522756402dca9664d0a432b1c084771
Maksyagin Aleksey <warmaxx@ya.ru>
2021-12-16 12:31:33 +0300

    admin news search

--- news/admin.py
+++ news/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import News
 
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("title", "date_create", "date_start", "date_end", )
+    search_fields = ("title",)
+    empty_value_display = "-пусто-"
+
+
 admin.site.register(News)
