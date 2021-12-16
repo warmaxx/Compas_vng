31355fd4887140b7a2cc2aa9bd1de8e196a1ad31
Maksyagin Aleksey <warmaxx@ya.ru>
2021-12-16 11:42:16 +0300

    admin File Folder search

--- vng_docs/admin.py
+++ vng_docs/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from .models import Folder, File
 
+
+class FileAdmin(admin.ModelAdmin):
+    list_display = ("name", "folder",)
+    search_fields = ("name",)
+    list_filter = ("folder",)
+    empty_value_display = "-пусто-"
+
+
 admin.site.register(Folder)
-admin.site.register(File)
+admin.site.register(File, FileAdmin)
