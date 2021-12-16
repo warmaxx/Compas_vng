c486a7fc173cdf118a2a33984c79523ae09cbc78
Maksyagin Aleksey <warmaxx@ya.ru>
2021-12-16 11:28:48 +0300

    admin region fedregion search

--- homepage/admin.py
+++ homepage/admin.py
@@ -3,7 +3,20 @@ from django.contrib import admin
 # Register your models here.
 from .models import FedRegion, Region
 
-admin.site.register(FedRegion)
-admin.site.register(Region)
+
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ("name", "fedname", "timezone",)
+    search_fields = ("name",)
+    list_filter = ("fedname",)
+    empty_value_display = "-пусто-"
+
+
+class FedRegionAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(FedRegion, FedRegionAdmin)
+admin.site.register(Region, RegionAdmin)
 admin.site.site_header = 'Администрирование Системы'
 admin.site.site_title = 'ИС ВНГ ГК'
