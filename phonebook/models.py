47c98ea315bd4fa8c14f7db6fcf6b2bda0e453a9
Maksyagin Aleksey <warmaxx@ya.ru>
2022-12-24 18:22:55 +0300

    clear migrations

--- phonebook/models.py
+++ phonebook/models.py
@@ -58,7 +58,7 @@ class Contact(models.Model):
 # def create_user_contact(sender, instance, created, **kwargs):
 #     if created:
 #         Contact.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_contact(sender, instance, **kwargs):
-    instance.contact.save()
+#
+# @receiver(post_save, sender=User)
+# def save_user_contact(sender, instance, **kwargs):
+#     instance.contact.save()
